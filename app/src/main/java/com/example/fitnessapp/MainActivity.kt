2a79201363ea9489b59136239fa6c8ad2edc54b7
Aditya-EUR1430 <aditya-eur1430@M-50s-MacBook-Air.local>
2025-09-05 15:03:25 +0530

    Added changes to read json file

--- app/src/main/java/com/example/fitnessapp/MainActivity.kt
+++ app/src/main/java/com/example/fitnessapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.fitnessapp
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -12,6 +14,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.fitnessapp.ui.theme.FitnessAppTheme
+import java.io.FileInputStream
+import java.nio.MappedByteBuffer
+import java.nio.channels.FileChannel
+import org.json.JSONArray
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,10 +30,39 @@ class MainActivity : ComponentActivity() {
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
+                    Log.d("MainActivity",
+                        loadModelFile(applicationContext, "exercise_model.tflite").toString()
+                    )
+                    val goalLabels = loadLabels(applicationContext, "goal_labels.json")
+                    val exerciseLabels = loadLabels(applicationContext, "exercise_labels.json")
+
+                    Log.d("MainActivity", "goal Label $goalLabels")
+                    Log.d("MainActivity", "goal Label $exerciseLabels")
+
                 }
             }
         }
     }
+
+    fun loadModelFile(context: Context, modelFileName: String): MappedByteBuffer {
+        val fileDescriptor = context.assets.openFd(modelFileName)
+        val inputStream = FileInputStream(fileDescriptor.fileDescriptor)
+        val fileChannel = inputStream.channel
+        val startOffset = fileDescriptor.startOffset
+        val declaredLength = fileDescriptor.declaredLength
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength)
+    }
+
+    fun loadLabels(context: Context, jsonFileName : String) : List<String>{
+        val json = context.assets.open(jsonFileName).bufferedReader().use { it.readText() }
+        val jsonArray = JSONArray(json)
+        val labels = mutableListOf<String>()
+        for (i in 0 until jsonArray.length()) {
+            labels.add(jsonArray.getString(i))
+        }
+        return labels
+    }
+
 }
 
 @Composable
